b7c1f0cca8c6d7fe45895261f398b738c4e2318a
lieforse <lieforse@gmail.com>
2020-09-18 15:28:49 +0300

    Updates

---
+++ precache-manifest.21518d38fcc51e30480dccd312c6eab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f9d08637249426500aa73ac8fd1f8b",
+    "url": "/road_to_mars/index.html"
+  },
+  {
+    "revision": "94a1c5572f40647543c1",
+    "url": "/road_to_mars/static/css/main.20745f97.chunk.css"
+  },
+  {
+    "revision": "213ac0bade28478d8455",
+    "url": "/road_to_mars/static/js/2.c1002582.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/road_to_mars/static/js/2.c1002582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a1c5572f40647543c1",
+    "url": "/road_to_mars/static/js/main.6eac2b51.chunk.js"
+  },
+  {
+    "revision": "e6c37a35f5d26967e8fb",
+    "url": "/road_to_mars/static/js/runtime-main.1042020c.js"
+  }
+]);
